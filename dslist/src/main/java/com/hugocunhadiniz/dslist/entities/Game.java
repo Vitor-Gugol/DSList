5e11c6219ea2a11ce36322c748874bd344c43c0b
Vitor <hugocunhadiniz@gmail.com>
2025-01-21 23:24:30 -0300

    Game Seed

--- dslist/src/main/java/com/hugocunhadiniz/dslist/entities/Game.java
+++ dslist/src/main/java/com/hugocunhadiniz/dslist/entities/Game.java
@@ -20,24 +20,28 @@ public class Game {
 	
 	@Column(name = "game_year")
 	private Integer year;
-	private String gender;
+	private String genre;
 	private String platforms;
 	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
 		
 	}
 
-	public Game(Long id, String title, Integer year, String gender, String platforms, Double score, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		
 		this.id = id;
 		this.title = title;
 		this.year = year;
-		this.gender = gender;
+		this.genre = genre;
 		this.platforms = platforms;
 		this.score = score;
 		this.imgUrl = imgUrl;
@@ -71,11 +75,11 @@ public class Game {
 	}
 
 	public String getGender() {
-		return gender;
+		return genre;
 	}
 
 	public void setGender(String gender) {
-		this.gender = gender;
+		this.genre = gender;
 	}
 
 	public String getPlatforms() {
